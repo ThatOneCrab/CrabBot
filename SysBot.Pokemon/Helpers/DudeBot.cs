fe6267b329420e7766735c0cb21b3b4429504dd0
ThatOneCrab <dannywhetzel@gmail.com>
2025-11-08 05:29:42 -0500

    version change

--- SysBot.Pokemon/Helpers/DudeBot.cs
+++ SysBot.Pokemon/Helpers/DudeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class CrabBot
     {
-        public const string Version = "2.2.3";
+        public const string Version = "1.2.3";
         public const string Attribution = "https://discord.gg/FnAGV4pb2k";
         public const string ConfigPath = "config.json";
     }
