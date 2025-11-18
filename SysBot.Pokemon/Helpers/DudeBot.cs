84c0b26175872803dcd12932bd846b91cc5e7ce5
ThatOneCrab <thatonecrab12@gmail.com>
2025-11-18 12:33:07 -0500

    .

--- SysBot.Pokemon/Helpers/DudeBot.cs
+++ SysBot.Pokemon/Helpers/DudeBot.cs
@@ -2,7 +2,7 @@ namespace SysBot.Pokemon.Helpers
 {
     public static class CrabBot
     {
-        public const string Version = "v1.2.5";
+        public const string Version = "v1.2.6";
         public const string Attribution = "https://discord.gg/FnAGV4pb2k";
         public const string ConfigPath = "config.json";
     }
