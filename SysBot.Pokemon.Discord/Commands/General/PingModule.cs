9275c5de357de2de8ea0afb82ba72923360872a5
ThatOneCrab <thatonecrab12@gmail.com>
2026-06-06 10:37:52 -0400

    Change to Ping embed

--- SysBot.Pokemon.Discord/Commands/General/PingModule.cs
+++ SysBot.Pokemon.Discord/Commands/General/PingModule.cs
@@ -1,5 +1,6 @@
 using Discord;
 using Discord.Commands;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace SysBot.Pokemon.Discord;
@@ -10,13 +11,28 @@ public class PingModule : ModuleBase<SocketCommandContext>
     [Summary("Makes the bot respond, indicating that it is running.")]
     public async Task PingAsync()
     {
-        var embed = new EmbedBuilder()
+        var stopwatch = Stopwatch.StartNew();
+
+        var initialEmbed = new EmbedBuilder()
             .WithTitle("Ping Response")
-            .WithDescription("Pong! Now stop @&#$?&! pinging me.")
-            .WithImageUrl("https://i.imgur.com/W9YmyA6.gif")
+            .WithDescription("Pong!")
+            .WithImageUrl("https://lparchive.org/Pokemon-Yellow/Update%2011/41-Trade_To.gif")
             .WithColor(Color.Green)
             .Build();
 
-        await ReplyAsync(embed: embed).ConfigureAwait(false);
+        var sentMessage = await ReplyAsync(embed: initialEmbed).ConfigureAwait(false);
+
+        stopwatch.Stop();
+        var elapsedMs = stopwatch.Elapsed.TotalMilliseconds;
+
+        var finalEmbed = new EmbedBuilder()
+            .WithTitle("Ping Response")
+            .WithDescription("Pong!")
+            .WithImageUrl("https://lparchive.org/Pokemon-Yellow/Update%2011/41-Trade_To.gif")
+            .WithColor(Color.Green)
+            .WithFooter($"Response time: {elapsedMs:F0} ms")
+            .Build();
+
+        await sentMessage.ModifyAsync(msg => msg.Embed = finalEmbed).ConfigureAwait(false);
     }
 }
